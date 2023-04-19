137318adf6b044c58599cd0c0d5364b0dcb20f4a
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-19 17:05:24 -0300

    adicionando labels no form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,30 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 
 function Login() {
-  const { queryData: { email, password }, handleChange, handleSubmit } = useContext(AppContext);
+  const {
+    queryData: { email, password },
+    handleChange,
+    handleSubmit,
+  } = useContext(AppContext);
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Login</h1>
       <form action="" method="post">
-        <input type="text" name="email" onChange={handleChange} value={email} />
-        <input type="text" name="password" onChange={handleChange} value={password} />
-        <button type="button" onClick={handleSubmit}>Enviar</button>
+        <label htmlFor="email-input">
+          Email:
+          {' '}
+          <input type="text" name="email" onChange={handleChange} value={email} />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          {' '}
+          <input type="text" name="password" onChange={handleChange} value={password} />
+        </label>
+        <button type="button" onClick={handleSubmit}>Entrar</button>
+        <button type="button" onClick={() => navigate('/register')}>Criar uma nova conta</button>
       </form>
     </div>
   );
