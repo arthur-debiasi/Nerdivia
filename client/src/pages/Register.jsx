b417dc4936b1e7ffa7881d916d8f24ce8531668c
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-19 17:05:32 -0300

    criando a página de register

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+
+function Register() {
+  const {
+    queryData: { email, password, name },
+    handleChange,
+    handleSubmit,
+  } = useContext(AppContext);
+  return (
+    <div>
+      <h1>Register</h1>
+      <form action="" method="post">
+        <label htmlFor="name-input">
+          Nome:
+          {' '}
+          <input id="name-input" type="text" name="name" onChange={handleChange} value={name} />
+        </label>
+        <label htmlFor="email-input">
+          Email:
+          {' '}
+          <input id="email-input" type="email" name="email" onChange={handleChange} value={email} />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          {' '}
+          <input type="password" name="password" onChange={handleChange} value={password} />
+        </label>
+        <button type="button" onClick={handleSubmit}>Criar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
