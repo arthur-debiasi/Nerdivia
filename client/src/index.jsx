138243c1618d68846a4abf8a35d28af181080d2d
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-19 15:40:44 -0300

    feat: criando index

---
+++ client/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import AppProvider from './context/AppProvider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
+reportWebVitals();
