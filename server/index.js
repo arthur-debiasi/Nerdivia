74613a3c1c4d72d875d3c2bee9cb64c2fe1b7cd3
unknown <sergioruza22@gmail.com>
2023-04-19 19:25:27 -0300

    fix: enviorment backend docker compose

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
